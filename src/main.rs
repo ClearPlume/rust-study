f25d367378f295370f44066cd2aadb188c4f49a0
the_FallenAngel <the.fallenangel.965@gmail.com>
2022-03-01 12:19:14 +0800

    变量的可变性

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 fn main() {
     define_variable();
+    mut_variable();
 }
 
 /// 变量的定义
 fn define_variable() {
-    // 创建一个数值对象“5”，将其所有者指定为“x”
+    // 创建一个数值对象“5”，将其所有者指定为x
     // 没有显式指定x的类型，则编译器自动推断
     let x = 5;
 
@@ -23,4 +24,23 @@ fn define_variable() {
     // 将格式化字符串输出到标准控制台
     // “{}”是一个占位符，会自动替换为sum的值
     println!("(x + y) + (i + j) = {}", sum);
+}
+
+/// 变量的可变性
+fn mut_variable() {
+    let x = 5;
+    println!("The value of x is: {}", x);
+
+    // 直接用let绑定的变量不可再更改值
+    // x = 6; // error: cannot assign twice to immutable variable
+    // println!("The value of x is: {}", x);
+
+    // 如需在初次绑定之后修改变量值，则在变量绑定时用mut修饰
+    // mut是mutable的缩写
+    let mut y = 5;
+    println!("The value of y is: {}", y);
+
+    // 创建对象“6”，绑定给变量y
+    y = 6;
+    println!("The value of y is: {}", y);
 }
